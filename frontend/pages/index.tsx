a8d4d114008c19f85ff88a4e84e4b3a83e8ef02d
Bonk Tracer <76239880+BonkTracer@users.noreply.github.com>
2025-04-29 19:42:45 +0700

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="container">
-      <h1>Chronos AI - Solana Whale & Anomaly Tracker</h1>
+      <h1>Bonk Trcaer - Solana Whale & Anomaly Tracker</h1>
       <Link href="/dashboard">
         <button>Go to Dashboard</button>
     </div>
